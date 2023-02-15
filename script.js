bb68a55098d17b0ea708744f563dbb6b5dc28917
Imransarkerweb <imransarker.web@gmail.com>
2023-02-15 14:38:34 +0600

    total section updated

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//variables
+const stateTax = 0.1;
+
+
+//functions
 function increamentor(id){
     const valueField =document.getElementById(id);
     const valueInNumber = parseInt(valueField.value);
@@ -24,6 +29,35 @@ function multiplier(price, qtyId){
     return price * qty
 }
 
+function getValuebyId(elementID){
+    const element = document.getElementById(elementID);
+    const value = parseInt(element.innerText)
+    return value
+}
+
+function setValuebyId(elementID, value){
+    const element = document.getElementById(elementID);
+    element.innerText = value;
+}
+
+function makeTotal(){
+    const getValuePhone = getValuebyId('phone-price')
+    const getValueCase = getValuebyId('case-price')
+    //total price
+    const totalPirce = getValueCase + getValuePhone
+
+    // total tax
+    const tax = parseFloat((totalPirce * stateTax).toFixed(2))
+
+    // grandTotal
+
+    const grandTotal = totalPirce + tax;
+    //setValue
+    setValuebyId('subtotal', totalPirce)
+    setValuebyId('tax', tax)
+    setValuebyId('grandTotal', grandTotal)
+}
+
 
 
 
@@ -31,12 +65,18 @@ document.getElementById('phone-plus-btn').addEventListener('click', function(){
     increamentor('phone-qty-field')
     const priceField =document.getElementById('phone-price')
     priceField.innerText= multiplier(1219, 'phone-qty-field')
+
+
+    makeTotal()
 })
 
 document.getElementById('phone-minus-btn').addEventListener('click', function(){
     decreamentor('phone-qty-field')
     const priceField =document.getElementById('phone-price')
     priceField.innerText= multiplier(1219, 'phone-qty-field')
+
+
+    makeTotal()
 })
 
 
@@ -45,10 +85,16 @@ document.getElementById('case-plus-btn').addEventListener('click', function(){
     increamentor('case-qty-field')
     const priceField =document.getElementById('case-price')
     priceField.innerText= multiplier(59, 'case-qty-field')
+
+
+    makeTotal()
 })
 
 document.getElementById('case-minus-btn').addEventListener('click', function(){
     decreamentor('case-qty-field')
     const priceField =document.getElementById('case-price')
     priceField.innerText= multiplier(59, 'case-qty-field')
+
+
+    makeTotal()
 })
